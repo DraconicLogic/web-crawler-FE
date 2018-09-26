45f9e881013f6ae1d79c5348347d53d291a924b6
andy hunt <andyhunt200@hotmail.com>
2018-09-26 11:39:55 +0100

    Implemented basic input functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Input from './components/Input';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Input />
       </div>
     );
   }
